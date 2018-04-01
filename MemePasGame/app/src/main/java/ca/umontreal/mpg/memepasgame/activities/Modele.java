cf8dba52093bbe7131413a22a4ca76a9370770cf
sarahgag <36801398+sarahgag@users.noreply.github.com>
2018-04-01 15:51:43 -0400

    drakefragment et modele2
    drake et modele2 (brain) fragement

--- MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/activities/Modele.java
+++ MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/activities/Modele.java
@@ -1,12 +1,10 @@
 package ca.umontreal.mpg.memepasgame.activities;
 
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
