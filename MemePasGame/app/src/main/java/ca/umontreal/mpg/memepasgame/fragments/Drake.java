3fe7e5d8756352e0113979c35530c2dba5a5ce21
sarahgag <36801398+sarahgag@users.noreply.github.com>
2018-04-01 16:02:11 -0400

    gossage
    du gossage

--- MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/fragments/Drake.java
+++ MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/fragments/Drake.java
@@ -13,12 +13,12 @@ import ca.umontreal.mpg.memepasgame.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link BlankFragment.OnFragmentInteractionListener} interface
+ * {@link Drake.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link BlankFragment#newInstance} factory method to
+ * Use the {@link Drake#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BlankFragment extends Fragment {
+public class Drake extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class BlankFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public BlankFragment() {
+    public Drake() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class BlankFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment Drake.
      */
     // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1, String param2) {
-        BlankFragment fragment = new BlankFragment();
+    public static Drake newInstance(String param1, String param2) {
+        Drake fragment = new Drake();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
