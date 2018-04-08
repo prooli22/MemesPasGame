e15576318beb2a65a72f77c670a975ef3a49d618
sarahgag <36801398+sarahgag@users.noreply.github.com>
2018-04-07 21:23:09 -0400

    effets boutons+pimpage de l'app

--- MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/activities/MainActivity.java
+++ MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/activities/MainActivity.java
@@ -1,31 +1,15 @@
 package ca.umontreal.mpg.memepasgame.activities;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 import ca.umontreal.mpg.memepasgame.R;
 
@@ -47,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
 
     public static Bitmap modifyOrientation(Bitmap bitmap, String image_absolute_path) throws IOException {
