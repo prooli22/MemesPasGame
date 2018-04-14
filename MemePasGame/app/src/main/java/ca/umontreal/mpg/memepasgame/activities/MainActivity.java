4d1b3a9894e73bc03797cf43c6e8ed75a20fd267
Moika <moika.sauve@outlook.fr>
2018-04-14 17:08:40 -0400

    menage

--- MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/activities/MainActivity.java
+++ MemePasGame/app/src/main/java/ca/umontreal/mpg/memepasgame/activities/MainActivity.java
@@ -1,28 +1,16 @@
 package ca.umontreal.mpg.memepasgame.activities;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.os.Bundle;
-import android.os.LocaleList;
 import android.support.v13.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.Toast;
 
-
-import java.util.Locale;
 
 import ca.umontreal.mpg.memepasgame.R;
 
